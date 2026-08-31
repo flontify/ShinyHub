d6eb4ccc26707dc53d8501c3ed2b94218da4baf8
flontify <arprocool@gmail.com>
2026-08-31 04:24:48 +0300

    Add files via upload

---
+++ loader.lua
@@ -0,0 +1,5 @@
+if game.PlaceId == 84637477108843 then --> Build An Ai
+    loadstring(game:HttpGet("https://pastefy.app/PhBnOAK5/raw"))()
+else
+    game.Players.LocalPlayer:Kick('lowkey we dont support this but [discord coming soon]')
+end
