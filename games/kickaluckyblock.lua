b94ba17de2b9b2c12a3952a34c54a2c32f9cf6b1
flontify <arprocool@gmail.com>
2026-09-08 01:21:03 +0300

    Add files via upload

--- games/kickaluckyblock.lua
+++ games/kickaluckyblock.lua
@@ -106,15 +106,30 @@ local function createSymbioteShim(gameName)
                 if type(default) ~= "boolean" then default = false end
                 if type(callback) ~= "function" then callback = function() end end
                 local flag = (tName .. "_" .. name):gsub("%s+","_")
-                local ok2, _ = pcall(function()
-                    return tab:CreateToggle({
+                local toggleObj
+                pcall(function()
+                    toggleObj = tab:CreateToggle({
                         Name = tName,
                         CurrentValue = default,
                         Flag = flag,
                         Callback = function(v) pcall(callback, v) end
                     })
                 end)
-                return { Set = function() end, SetValue=function() end }
+                local function setToggle(v)
+                    if toggleObj then
+                        pcall(function()
+                            if toggleObj.Set then toggleObj:Set(v)
+                            elseif toggleObj.SetValue then toggleObj:SetValue(v)
+                            end
+                        end)
+                    end
+                end
+                return {
+                    Set = setToggle,
+                    SetValue = setToggle,
+                    SetState = setToggle,
+                    SetEnabled = setToggle
+                }
             end
             function TabObj:AddSlider(sName, min, max, def, callback)
                 if type(callback) ~= "function" then callback = function() end end
